ccd1db130ca627b2953c7f2886081750863817a5
CollioTV <collio_v@epitech.eu>
2014-05-20 19:50:23 +0200

    Phisix + Collider

--- bomberman/includes/Component.hh
+++ bomberman/includes/Component.hh
@@ -2,9 +2,49 @@
 # define __COMPONENT_H__
 
 namespace Component{
+  class Collider{
+  public:
+    class Object{
+    public:
+      typedef unsigned int Id;
+    };
+  public:
+    Collider(Event::Dispatcher*);
 
+  private:
+    std::list< Object* >	collideList;
+  public:
+    Object::Id	addColliderObject(int x, int y);
+    void	moveColliderObject(Object::Id, int x, int y);
 
+  public:
+    bool	operator()(int x, int y);
+  };
 
+  class Phisix{
+  public:
+    class Vector{
+    public:
+      Vector(Phisix*);
+
+    public:
+      enum Direction{ Up, Down, Left, Right};
+      void	modifyMovement(enum Direction, bool mask);
+      void	modifySpeed(double speed);
+      void	setSpeed(double speed);
+      void	authorizeMovement(bool);
+
+    public:
+      void	move(void);
+      void	getPosition(int& x, int& y);
+    };
+
+  public:
+    Phisix(Event::Dispatcher*, const Collider&);
+
+  public:
+    void	modifyFisix(/* ??? */);
+  };
 };
 
 #endif
