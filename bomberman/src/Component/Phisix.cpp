4a353d929403fae1ecbeadbda474d7da24b3a270
CollioTV <collio_v@epitech.eu>
2014-05-20 21:21:49 +0200

    avancée sur Phisix

--- bomberman/src/Component/Phisix.cpp
+++ bomberman/src/Component/Phisix.cpp
@@ -15,7 +15,7 @@ namespace Component
   /*  Vector  */
 
   Phisix::Vector::Vector(Phisix *Phisik, int x, int y, double _speed = 1)
-    : speed(_speed), myPhisix(Phisik), movementDirection({false, false, false, false}), 
+    : speed(_speed), myPhisix(Phisik), movementDirection({false, false, false, false}),
       authorization(true), positionX(x), positionY(y) {}
   
   void		Phisix::Vector::modifyMovement(enum Direction, bool mask)
@@ -38,7 +38,28 @@ namespace Component
 
   void	    Phisix::Vector::move()
   {
-    
+    double	x = 0;
+    double	y = 0;
+
+    if (!autorization)
+      return ;
+    if (movementDirection[Up])
+      y -= this->speed * this->myPhisix->getGlobalFriction();
+    if (movementDirection[Down])
+      y += this->speed * this->myPhisix->getGlobalFriction();
+    if (movementDirection[Right])
+      x += this->speed * this->myPhisix->getGlobalFriction();
+    if (movementDirection[Left])
+      x -= this->speed * this->myPhisix->getGlobalFriction();
+    if (!this->phisix->getCollider()(this->positionX + x, this->positionY + y)){
+      if (x != 0 && this->phisix->getCollider()(this->positionX + x, this->positionY)) {
+	this->positionX += x;
+      } else if (y != 0 && !this->phisix->getCollider()(this->positionX, this->positionY + y)){
+	this->postionY += y;
+      } else { return ; }
+    }
+    this->positionX += x;
+    this->positionY += y;
   }
 
   void	    Phisix::Vector::getPosition(int& x, int& y)
