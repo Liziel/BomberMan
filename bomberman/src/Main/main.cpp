74384b5cef2036d99b11932f3db0110cdf2bff42
CollioTV <collio_v@epitech.eu>
2014-06-11 16:18:34 +0200

    wut mon main a suter? (see commits)

--- bomberman/src/Main/main.cpp
+++ bomberman/src/Main/main.cpp
@@ -2,17 +2,6 @@
 # include	<iostream>
 # include	"GameEngine.hh"
 # include	"Tokenizer.hpp"
-# include	"GameEngine.hpp"
 
 int main()  /* Main de test */
-{
-	GameEngine engine;
-
-	if (engine.initialize() == false)
-		return (EXIT_FAILURE);
-
-	while (engine.update() == true)
-		engine.draw();
-
-	return EXIT_SUCCESS;
-}
+{}
