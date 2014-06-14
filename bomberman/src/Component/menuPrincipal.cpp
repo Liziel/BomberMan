ee1b765090e96bcf471d172707f5c3d7ef2beabc
dumont_s <dumont_s@epitech.eu>
2014-06-14 20:50:52 +0200

    fix menu compilation

--- bomberman/src/Component/menuPrincipal.cpp
+++ bomberman/src/Component/menuPrincipal.cpp
@@ -2,9 +2,9 @@
 
 namespace Component {
 
-  MenuPrincipal::MenuPrincipal(Entity::GameObject* _p, Engine::Graphic* g, int sizeX, int sizeY, int posX, int posY, const std::string& texture) : Menu(_p, g, sizeX, sizeY, posX, posY)
+  MenuPrincipal::MenuPrincipal(Entity::GameObject* _p, Engine::Graphic* g, int sizeX, int sizeY, int posX, int posY, const std::string& texture) : Menu(_p, g, sizeX, sizeY, posX, posY, texture)
   {
-    addButton(400, 150, WINDOW_X / 2 - 200, 100, "buttons/play.tga", true);
+    addButton(g, 400, 150, WINDOW_X / 2 - 200, 100, "buttons/play.tga", "buttons/play_pressed.tga", true);
     draw();
   }
   
